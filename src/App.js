133dfec725b6766893eb1c5cd706deaeb409ade8
dobyming <12184874@inha.edu>
2023-01-28 14:28:36 +0900

    Add List Rendering

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import './App.css';
 import DiaryEditor from './DiaryEditor';
+import DiaryList from './DiaryList';
 
-function App() {
+const dummyList = [
+  {
+    id: 1,
+    author:"김다민",
+    content: "반갑습니다!",
+    emotion: 1,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    author:"배주현",
+    content: "반갑습니다",
+    emotion: 3,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 3,
+    author:"강슬기",
+    content: "반갑습니닿!",
+    emotion: 2,
+    created_date: new Date().getTime(),
+  }
+]
+
+const App = () => {
   return (
     <div className="App">
       <DiaryEditor/>
+      <DiaryList diaryList={dummyList}/>
     </div>
   );
 }
