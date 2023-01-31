49f3a71f97c99b682b840a53a69f9f7bd94065ca
dobyming <12184874@inha.edu>
2023-01-31 17:33:20 +0900

    rendering Diary by Calling API-useEffect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import { useRef,useState } from 'react';
+import { useEffect, useRef,useState } from 'react';
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
+
+//https://jsonplaceholder.typicode.com/comments
 
 const App = () => {
   const [data,setData] = useState([]); //현재 state인 data를 diaryList로 넘기고, 상태변화는 Editor에서 props로 넘겨져 이뤄진다.
 
   const dataId = useRef(0); //id를 리스트에 넘겨줘야 함 
+
+  //API 호출하기
+  const getData = async() => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/comments'
+    ).then((res)=>res.json());
+    
+    const initData = res.slice(0,20).map((it)=>{
+      return {
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random()*5)+1,
+        created_date: new Date().getTime(),
+        id: dataId.current++
+      }
+    });
+
+    setData(initData);
+  }
+
+  //mount 되자마자 API call하기
+  useEffect(()=>{
+    getData();
+  },[])
 
   //일기 생성 기능
   const onCreate = (author,content,emotion) => {
