0647aca231b14c2dc6ccdc2686727cf25a6b8966
dobyming <12184874@inha.edu>
2023-02-05 21:19:44 +0900

    manage state functions with useReducer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-import { useMemo, useEffect, useRef,useState, useCallback } from 'react';
+import { useMemo, useEffect, useRef, useCallback, useReducer } from 'react';
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
 
+const reducer = (state,action) => {
+  switch(action.type) {
+    case 'INIT':{
+      return action.data;
+    }
+    case 'CREATE':{
+      const created_date = new Date().getTime();
+      const newItem = {
+        ...action.data, //onCreate에서 setting한 값 spread
+        created_date
+      }
+      return [newItem,...state]; 
+    }
+    case 'REMOVE':{
+      return state.filter((it)=> it.id !== action.targetId);
+    }
+    case 'EDIT':{
+      return state.map((it)=>
+      it.id === action.targetId ? 
+      {...it,content: action.newContent} : it)
+    }
+    default:
+      return state;
+  }
+}; //상태변화 함수 관리(state:이전 상태, action:switch문에 해당하는 action 처리)
+
 const App = () => {
-  const [data,setData] = useState([]); //현재 state인 data를 diaryList로 넘기고, 상태변화는 Editor에서 props로 넘겨져 이뤄진다.
+  // const [data,setData] = useState([]); //현재 state인 data를 diaryList로 넘기고, 상태변화는 Editor에서 props로 넘겨져 이뤄진다.
+  const [data,dispatch] = useReducer(reducer,[]); //상태변화 함수 리팩토링(App 컴포넌트 밖에다가 reduecer 정의)을 위한 useReducer hook 사용
 
   const dataId = useRef(0); //id를 리스트에 넘겨줘야 함 
 
@@ -22,8 +49,7 @@ const App = () => {
         id: dataId.current++
       }
     });
-
-    setData(initData);
+    dispatch({type:'INIT',data:initData}); //'INIT'이라는 액션으로 받아, data에 initData로 초기화
   }
 
   //mount 되자마자 API call하기
@@ -34,27 +60,21 @@ const App = () => {
   //일기 생성 기능
   const onCreate = useCallback(
     (author,content,emotion) => {
-    const created_date = new Date().getTime();
-    const newItem = {
-      author,
-      content,
-      emotion,
-      created_date,
-      id: dataId.current
-    }
+    dispatch({
+      type:'CREATE',
+      data:{author,content,emotion, id:dataId.current }
+    });
     dataId.current += 1;
-    setData((data)=>[newItem,...data]); //newItem이 맨 위에 추가되도록 setData 매개변수로 전달 및 초기 빈배열 세팅 트러블 슈팅(함수형 update)
   },[]); //불필요한 호출을 useCallback()을 통해 최적화
 
   //일기 삭제 기능(id를 기준으로 삭제)
-  const onRemove = useCallback(
-    (targetId) => {
-    setData((data)=>data.filter((it)=> it.id !== targetId)); //최신 state 전달
+  const onRemove = useCallback((targetId) => {
+    dispatch({type:'REMOVE',targetId});
   },[]);
 
   //일기 수정 기능
   const onEdit = useCallback((targetId,newContent) => {
-    setData((data) =>data.map((it)=> it.id === targetId ? {...it,content: newContent} : it))
+    dispatch({type:'EDIT',targetId,newContent})
   },[]);
 
   const getDiaryAnalysis = useMemo(()=> {
